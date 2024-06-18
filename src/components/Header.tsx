15241fccf768d391c43d8430ad8694f18b65892a
Israel Malagutti <israelmalagutti@gmail.com>
2024-06-18 10:26:42 -0300

    chore: add border bottom to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,11 @@ import { ThemeToggle } from "@/components/ui/ThemeToggle";
 
 export function Header() {
   return (
-    <div className="min-w-full flex items-center justify-between px-12 py-4">
+    <div className="flex min-w-full items-center justify-between border-b-[1px] border-[hsl(var(--border))] px-12 py-4">
       <h1 className="text-lg font-bold text-pink-600 dark:text-pink-400">
         Louvores IBC/JUBAC
       </h1>
+
       <ThemeToggle />
     </div>
   );
